c6376f8a2ace7a09ad0c25dc698bdcd1130b8dd9
Joyonta Saha <joyontasaha8@gmail.com>
2021-08-24 00:01:55 +0600

    studentSequence created and generated

--- src/main/java/com/joyontasaha/spring/data/jpa/tutorial/entity/Student.java
+++ src/main/java/com/joyontasaha/spring/data/jpa/tutorial/entity/Student.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "StudentTable")
@@ -19,6 +16,15 @@ import javax.persistence.Table;
 public class Student {
 
     @Id
+    @SequenceGenerator(
+            name = "studentSequence",
+            sequenceName = "studentSequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "studentSequence"
+    )
     private Long studentId;
     private String firstName;
     private String lastName;
