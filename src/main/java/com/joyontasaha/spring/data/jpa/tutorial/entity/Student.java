516be17bb68d8d3d3d7193ea62f8b955170160e6
Joyonta Saha <joyontasaha8@gmail.com>
2021-08-23 23:37:56 +0600

    initial commit

---
+++ src/main/java/com/joyontasaha/spring/data/jpa/tutorial/entity/Student.java
@@ -0,0 +1,25 @@
+package com.joyontasaha.spring.data.jpa.tutorial.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Student {
+
+    @Id
+    private Long studentId;
+    private String firstName;
+    private String lastName;
+    private String emailId;
+    private String guardianName;
+    private String guardianEmail;
+    private String guardianMobile;
+}
