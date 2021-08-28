37d32ad73201f983b8027fd2db0dc2cbab167a17
Joyonta Saha <joyontasaha8@gmail.com>
2021-08-28 15:21:16 +0600

    many to many relationship implemented and tested using student, guardian, teacher, course, course materials entities

--- src/test/java/com/joyonta/spring/data/jpa/tutorial/repository/CourseMaterialRepositoryTest.java
+++ src/test/java/com/joyonta/spring/data/jpa/tutorial/repository/CourseMaterialRepositoryTest.java
@@ -1,7 +1,6 @@
 package com.joyonta.spring.data.jpa.tutorial.repository;
 
-import com.joyonta.spring.data.jpa.tutorial.entity.Course;
-import com.joyonta.spring.data.jpa.tutorial.entity.CourseMaterial;
+import com.joyonta.spring.data.jpa.tutorial.entity.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -38,5 +37,41 @@ class CourseMaterialRepositoryTest {
     public void printCourseMaterialById() {
         CourseMaterial courseMaterial = courseMaterialRepository.findById(2L).get();
         System.out.println("courseMaterial = " + courseMaterial);
+    }
+    @Test
+    public void saveCourseMaterialWithCourseAndStudentAndGuardianAndTeacher() {
+        Guardian guardian = Guardian.builder()
+                .email("Sagnik@gmail.com")
+                .name("Nikhil")
+                .mobile("9999956324")
+                .build();
+
+        Student student = Student.builder()
+                .firstName("Kalyan")
+                .emailId("Kalyan@gmail.com")
+                .lastName("Kumar")
+                .guardian(guardian)
+                .build();
+
+        Teacher teacher = Teacher.builder()
+                .firstName("Sarthi")
+                .lastName("Bhattachariya")
+                .build();
+
+        Course course = Course.builder()
+                .title("Discrete Math")
+                .credit(8)
+                .teacher(teacher)
+                .build();
+
+        course.addStudent(student);
+
+        CourseMaterial courseMaterial = CourseMaterial.builder()
+                .url("www.tutorial.com/discrete-math")
+                .course(course)
+                .build();
+
+
+        courseMaterialRepository.save(courseMaterial);
     }
 }
