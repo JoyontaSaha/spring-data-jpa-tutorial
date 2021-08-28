866b88c5400cdd698d5b7fc01e39065446975a2c
Joyonta Saha <joyontasaha8@gmail.com>
2021-08-28 10:50:29 +0600

    one to many relationship implemented and tested using Teacher, Course entity

---
+++ src/test/java/com/joyonta/spring/data/jpa/tutorial/repository/TeacherRepositoryTest.java
@@ -0,0 +1,38 @@
+package com.joyonta.spring.data.jpa.tutorial.repository;
+
+import com.joyonta.spring.data.jpa.tutorial.entity.Course;
+import com.joyonta.spring.data.jpa.tutorial.entity.Teacher;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class TeacherRepositoryTest {
+    @Autowired
+    private TeacherRepository repository;
+
+    @Test
+    public void saveTeacher() {
+        Course dsaCourse = Course.builder()
+                .title("DSA")
+                .credit(6)
+                .build();
+        Course caCourse = Course.builder()
+                .title("CA")
+                .credit(6)
+                .build();
+        Teacher teacher = Teacher.builder()
+                .firstName("Moumita")
+                .lastName("Bhattachariya")
+                .courses(Arrays.asList(dsaCourse, caCourse))
+                .build();
+
+        repository.save(teacher);
+    }
+
+
+}
